7ec3331a4c3fdd67be8e6807cdcfce118dec5df1
Sebastien Wieckowski <sbwiecko@free.fr>
2020-07-02 14:31:01 +0200

    ex020,040

--- 030-Avancé/020-Calculer l'aire d'un rectangle avec un ContextManager/source.py
+++ 030-Avancé/020-Calculer l'aire d'un rectangle avec un ContextManager/source.py
@@ -2,6 +2,17 @@ class Rectangle:
     def __init__(self, longueur, largeur):
         self.longueur = longueur
         self.largeur = largeur
+    
+    def calcul_aire(self):
+        print(f"{self.longueur * self.largeur}m²")
+    
+    def __enter__(self):
+        print(f"L'aire d'un rectangle de {self.longueur}m par {self.largeur}m est de :")
+        return self # pour l'application de la méthode dans le context manager
+        
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+    
 
 with Rectangle(6, 12) as r:
     r.calcul_aire()
--- 030-Avancé/040-Gérer une classe d'élèves/source.py
+++ 030-Avancé/040-Gérer une classe d'élèves/source.py
@@ -2,10 +2,18 @@ class Eleve:
     def __init__(self, prenom, nom):
         self.prenom = prenom
         self.nom = nom
+    
+    def __repr__(self):
+        return "{} {}".format(self.prenom, self.nom)
 
 
 class Classe:
-    pass
+    
+    eleves = [] # liste des élèves
+    
+    @staticmethod
+    def ajouter_eleve(prenom, nom):
+        Classe.eleves.append(Eleve(prenom, nom))
 
 
 Classe.ajouter_eleve("John", "Smith")
